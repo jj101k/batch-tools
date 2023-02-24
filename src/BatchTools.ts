32ded4b71a1a489d5cfd714f6419b86436efde27
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-02-24 17:39:05 +0000

    Improve doc

--- src/BatchTools.ts
+++ src/BatchTools.ts
@@ -180,6 +180,15 @@ export class BatchTools<T, U> {
 
     }
 
+    /**
+     * Adds multiple items to one or more batches. Returns when done.
+     *
+     * Important note: this will only be done when all of them are, which means
+     * you'll probably have to wait for the timeout.
+     *
+     * @param ts
+     * @returns
+     */
     async callMulti(...ts: T[]): Promise<U[]> {
         if(!this.activeBatch) {
             this.activeBatch = new Batch(this.func, this.sendCondition)
