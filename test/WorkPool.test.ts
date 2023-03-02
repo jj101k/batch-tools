b18e75f5e1a769fdae092d3c1fd9348c1c800b53
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-03-02 08:27:59 +0000

    Test loop avoidance

--- test/WorkPool.test.ts
+++ test/WorkPool.test.ts
@@ -9,12 +9,23 @@ describe("Work pool", () => {
     it("has a maximum size", async () => {
         let c = 0
         const action = () => {c++; return new Promise(resolve => setTimeout(resolve, 100))}
-        for(let i = 0; i < 10; i++) {
+        for(let i = 0; i < 8; i++) {
             pool.add(action)
         }
         await TestHelper.pause(50)
         assert.equal(c, 5, "only the work that fitted in the pool is complete")
         await TestHelper.pause(100)
-        assert.equal(c, 10, "all work is eventually complete")
+        assert.equal(c, 8, "all work is eventually complete")
+    })
+    it("has a maximum rate", async () => {
+        let c = 0
+        const action = () => {c++}
+        for(let i = 0; i < 20; i++) {
+            pool.add(action)
+        }
+        await TestHelper.pause(50)
+        assert.equal(c, 10, "only the work within the work rate is complete")
+        await TestHelper.pause(100)
+        assert.equal(c, 20, "all work is eventually complete")
     })
 })
