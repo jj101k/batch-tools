56598b38844168009ea4fb39cd8bf16643d1b90c
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-09-08 08:14:01 +0100

    Be more specific about them corresponding correctly

--- test/BatchRemovable.test.ts
+++ test/BatchRemovable.test.ts
@@ -7,18 +7,19 @@ const debug = false
 describe("BatchRemovable is usable", () => {
     it("runs normally when all items are added at once", async () => {
         const batch = new BatchRemovable<number, number>((...ts) => Promise.resolve(ts), {timeoutMs: 50})
-        const resultsOut: (number | undefined)[] = []
+        const resultsOut = new Map<number, number | undefined>()
         for(let i = 0; i < 30; i+=2) {
             const result = batch.add(i, i + 1)
             assert.equal(result.remaining, 0, `Item ${i} is accepted`)
-            result.promise.then(r => {resultsOut.push(r[0]); resultsOut.push(r[1])})
+            const j = i
+            result.promise.then(r => {resultsOut.set(j, r[0]); resultsOut.set(j + 1, r[1])})
         }
         for(let i = 20; i < 30; i+=2) {
             batch.remove(i, i + 1)
         }
         await new Timeout(100)
-        assert.equal(resultsOut.length, 30, "All results in")
-        assert.equal(new Set(resultsOut).size, 21, "All results are distinct")
-        assert.equal(resultsOut.filter(r => r !== undefined && r < 20).length, 20, "Only the expected results exist")
+        assert.equal(resultsOut.size, 30, "All results in")
+        assert.equal(new Set(resultsOut.values()).size, 21, "All results are distinct")
+        assert.equal([...resultsOut].filter(([k, v]) => v !== undefined && v < 20 && k == v).length, 20, "Only the expected results exist")
     })
 })
