d960975ea7a4c748a4ca16b939e032b1c0dd8c15
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-09-08 08:17:21 +0100

    Improved testing

--- test/BatchRemovable.test.ts
+++ test/BatchRemovable.test.ts
@@ -5,7 +5,7 @@ import { BatchRemovable } from "../src/LowLevel/BatchRemovable"
 const debug = false
 
 describe("BatchRemovable is usable", () => {
-    it("runs normally when all items are added at once", async () => {
+    it("runs normally when all items are added at once and the end is removed", async () => {
         const batch = new BatchRemovable<number, number>((...ts) => Promise.resolve(ts), {timeoutMs: 50})
         const resultsOut = new Map<number, number | undefined>()
         for(let i = 0; i < 30; i+=2) {
@@ -21,5 +21,28 @@ describe("BatchRemovable is usable", () => {
         assert.equal(resultsOut.size, 30, "All results in")
         assert.equal(new Set(resultsOut.values()).size, 21, "All results are distinct")
         assert.equal([...resultsOut].filter(([k, v]) => v !== undefined && v < 20 && k == v).length, 20, "Only the expected results exist")
+    })
+
+    it("runs normally with random removals", async () => {
+        const batch = new BatchRemovable<number, number>((...ts) => Promise.resolve(ts), {timeoutMs: 50})
+        const resultsOut = new Map<number, number | undefined>()
+        for(let i = 0; i < 30; i+=2) {
+            const result = batch.add(i, i + 1)
+            assert.equal(result.remaining, 0, `Item ${i} is accepted`)
+            const j = i
+            result.promise.then(r => {resultsOut.set(j, r[0]); resultsOut.set(j + 1, r[1])})
+        }
+        let removed = 0
+        for(let i = 0; i < 30; i++) {
+            if(Math.random() > 0.5) {
+                removed++
+                batch.remove(i)
+            }
+        }
+        await new Timeout(100)
+        const expected = 30 - removed
+        assert.equal(resultsOut.size, 30, "All results in")
+        assert.equal(new Set(resultsOut.values()).size, expected + 1, "All results are distinct")
+        assert.equal([...resultsOut].filter(([k, v]) => v !== undefined && k == v).length, expected, "Only the expected results exist")
     })
 })
